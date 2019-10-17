e2b03f107709ab06bddc6696761b75232d631d0b
Sora Olaya Yovany <ysorao@heon.com.co>
2019-10-16 21:09:37 -0500

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { FormsModule } from '@angular/forms';
 import { APP_ROUTING } from './app.routes';
 
 //Servicios
-
+import { HeroesService} from './services/heroes.service';
 
 //Componentes
 import { AppComponent } from './app.component';
@@ -15,6 +15,7 @@ import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 @NgModule({
   declarations: [
@@ -22,14 +23,17 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     HomeComponent,
     NavbarComponent,
     AboutComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../../services/heroes.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+   heroes: Heroe[]=[];
+
+  constructor(private _heroesService:HeroesService) { }
 
   ngOnInit() {
+    this.heroes = this._heroesService.getHeroes();
+
+    console.log(this.heroes);
+
+  }
+
+  verHeroe(idx:number){
+    console.log(idx);
   }
 
 }
